7fac7a313f5fd848bfcc783afdb3c8e6fda956f4
unknown <01nirakarbhattarai@gmail.com>
2024-06-17 16:23:53 +0545

    Navbar aadhi complete

--- FRONTEND/src/Components/Navbar/Navbar.jsx
+++ FRONTEND/src/Components/Navbar/Navbar.jsx
@@ -1,20 +1,25 @@
 import React from 'react'
 import logo from '../assets/logo.png'
 import cart_icon from '../assets/cart_icon.png'
+import { useState } from 'react'
 import './Navbar.css'
 
 const Navbar = () => {
+
+    const [menu, setMenu] =  useState("home") ;
+
+
   return (
     <div className='navbar'>
-        <div className="nav-logg">
+        <div className="nav-logo">
             <img src={logo} alt="" />
             <p>Name</p>
         </div>
         <ul className="nav-menu">
-            <li>Home</li>
-            <li>Men</li>
-            <li>Women</li>
-            <li>Kids</li>
+            <li onClick={()=> {setMenu("home")}} >Home<hr/></li>
+            <li onClick={()=> {setMenu("men")}} >Men</li>
+            <li onClick={()=> {setMenu("women")}} >Women</li>
+            <li onClick={()=> {setMenu("kids")}} >Kids</li>
         </ul>
         <div className="nav-cart-menu">
             <button className="loginBtn">Log In</button>
